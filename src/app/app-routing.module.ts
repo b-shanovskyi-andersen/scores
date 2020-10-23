839f7148ba5d50b6c21de15c9fa843ffdc0d7bde
b-shanovskyi-andersen <b.shanovskyi@andersenlab.com>
2020-10-23 12:05:00 +0300

    feat(routin): add pages routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ListPageComponent } from './list-page/list-page.component';
+import { PersonDetailsPageComponent } from './person-details-page/person-details-page.component';
+
+const LIST_PATH = 'list';
+const PERSON_DETAILS_PATH = '';
+
+const routes: Routes = [
+  { 
+    path: LIST_PATH, 
+    component: ListPageComponent,
+    pathMatch: 'full'
+  },
+  { 
+    path: `${LIST_PATH}/:id`, 
+    component: PersonDetailsPageComponent},
+  {
+    path: '',
+    redirectTo: LIST_PATH,
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ListPageComponent } from './list-page/list-page.component';
+import { PersonDetailsPageComponent } from './person-details-page/person-details-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPageComponent,
+    PersonDetailsPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
