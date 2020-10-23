d27c6633a58deac9807344620dd7f04f08bc6e82
b-shanovskyi-andersen <b.shanovskyi@andersenlab.com>
2020-10-23 17:50:52 +0300

    feat(displaying-person): dispaly selected person JSON

--- src/app/pages/person-details-page/components/person-details-page/person-details-page.component.ts
+++ src/app/pages/person-details-page/components/person-details-page/person-details-page.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { PeopleService } from 'src/app/core/people/people.service';
+import { Person } from 'src/app/shared/models/person.interface';
 
 @Component({
   selector: 'app-person-details-page',
   templateUrl: './person-details-page.component.html',
   styleUrls: ['./person-details-page.component.scss']
 })
-export class PersonDetailsPageComponent {
+export class PersonDetailsPageComponent implements OnInit {
+  person$: Observable<Person>;
+  
+  constructor(private peopleService: PeopleService, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.activatedRoute.params
+      .pipe(
+        take(1)
+      )
+      .subscribe(params => {
+        this.person$ = this.peopleService.getById(Number(params.id))
+      })
+  }
 }
